c027c3eca384f1b885f67f9d710c57428d6e284a
pikapika2 <yossii0108@gmail.com>
2019-05-24 21:00:21 +0900

    add

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -37,6 +37,8 @@ function getnow(){
 var img = new Array();
 var imgr = new Array();
 var koko = "";			//ドラッグ中の画像のlogを格納
+var liked = 0;
+var flag_fairy = false;
 
 //画像の場所を格納させる関数(今回はthisを使いたかっただけ)
 function Picture(n){
@@ -52,6 +54,8 @@ img[2] = new Picture("img/dragon_red.png");
 img[3] = new Picture("img/dragon_blue.png");
 img[4] = new Picture("img/dragon_black.png");
 img[5] = new Picture("img/dragon_white.png");
+img[6] = new Picture("img/blackdog.png");
+img[7] = new Picture("img/creamdog.png");
 
 //条件分岐のための配列
 
@@ -60,16 +64,42 @@ imgr[1] = "kaminari.png";
 imgr[2] = "fire.png";
 imgr[3] = "suisosui.png";
 imgr[4] = "alice.png";
+imgr[5] = "heart.png";
+imgr[6] = "pinkcd.png";
+imgr[7] = "bokujou.png";
 
 //html上のモンスター画像の変更処理
 
 function changeIMG(){
+	if(document.getElementById("gazo").src==img[0].n.src){   //イーブイの時に進化可能にする
+		switch_eievui();
+	}
+	if(koko==imgr[0]){     //ただの石だったら無条件でイーブイに
+		document.getElementById("gazo").src=img[0].n.src;
+		liked = 0;
+		flag_fairy = false;
+	} 
+}
+
+function switch_eievui(){
 	switch(koko){
-		case imgr[0] : document.getElementById("gazo").src=img[0].n.src; break;
 		case imgr[1] : document.getElementById("gazo").src=img[1].n.src; break;
 		case imgr[2] : document.getElementById("gazo").src=img[2].n.src; break;
 		case imgr[3] : document.getElementById("gazo").src=img[3].n.src; break;
-		case imgr[4] : timeChoice(); break;
+		case imgr[4] : likedChoice(); break;
+		case imgr[5] : liked++; break;
+		case imgr[6] : flag_fairy=true; break;
+		case imgr[7] : positionChoice(); break;
+	}
+}
+
+function likedChoice(){
+	if(flag_fairy == true){
+		if(liked >= 2){
+			document.getElementById("gazo").src=img[6].n.src;
+		}
+	}else if(liked >= 5){
+		timeChoice();
 	}
 }
 
@@ -83,10 +113,39 @@ function timeChoice(){
 
 }
 
-//　ドラッグ中の画像のlogの検出
 
+//　ドラッグ中の画像のlogの検出と保存
 document.getElementById("rock").ondragstart = function(e){
 
 	koko = e.target.currentSrc.split("/").pop();
 	console.log(koko);   //ログがちゃんと取れているかの確認用  
+}
+
+function positionChoice(){
+
+	//位置情報検出とその後の分岐
+	navigator.geolocation.getCurrentPosition( successFunc , errorFunc ) ;
+	function successFunc( position ){
+		var pos = position.coords.latitude + position.coords.longitude;
+		console.log(pos); //確認
+		if(pos*1000 % 2 == 0){
+			document.getElementById("gazo").src=img[6].n.src;
+		}else{
+			document.getElementById("gazo").src=img[7].n.src;
+		}
+	}		
+	function errorFunc( error ){
+		// エラーコードのメッセージを定義
+		var errorMessage = {
+			0: "原因不明のエラー、どうしようもない。" ,
+			1: "位置情報の取得を許可してないの！？。" ,
+			2: "電波状況などで位置情報が取得できないよ、通信状況よくしてねー。" ,
+			3: "位置情報の取得に時間がかかり過ぎてタイムアウトしました。" ,
+		} ;
+
+		// エラーコードに合わせたエラー内容をアラート表示
+		alert( errorMessage[error.code] ) ;
+		console.log(errorMessage[error.code]);
+		console.log(error);
+	}
 }
